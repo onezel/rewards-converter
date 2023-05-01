1adf308e3387a069ec552f9c6b13bd39cf3fc728
Zelnick <117078615+onezel@users.noreply.github.com>
2023-05-01 16:54:22 -0700

    Task 2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,13 @@
 public class RewardValue {
     private double cashValue;
-    private double milesValue;
+    private int milesValue;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035;
+        this.milesValue =(int) (cashValue / 0.0035);
     }
 
-    public RewardValue(double milesValue, boolean isMiles) {
+    public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         this.cashValue = milesValue * 0.0035;
     }
@@ -16,7 +16,7 @@ public class RewardValue {
         return cashValue;
     }
 
-    public double getMilesValue() {
+    public int getMilesValue() {
         return milesValue;
     }
 }
